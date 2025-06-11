28cb4c6ed82e3a1f866256bfb8c0127ce01efe62
BrunoMaiaPinto <brunommpinto@sapo.pt>
2025-06-11 15:12:17 +0100

    Add form submission alert and clean up garage selection in inserir.html

--- radarimovel/static/radarimovel/script.js
+++ radarimovel/static/radarimovel/script.js
@@ -1,1 +1,7 @@
+const form = document.querySelector(".formulario");
+
+form.addEventListener("submit", function (e) {
+  alert("Imovél adicionado com sucesso!");
+});
+
 document.getElementById("year").innerHTML = `${new Date().getFullYear()}`;
